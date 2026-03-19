d360f30a2ccce1467f71a1d1f20ca8fbaf5d2111
chris091109 <chrisventer999@gmail.com>
2026-03-19 18:09:40 +0800

    update shaders and made the rendering obj a rectangle

--- source/drivers/render.cc
+++ source/drivers/render.cc
@@ -5,13 +5,21 @@
 namespace Engine {
 
 const std::vector<Vertex> vertices = {
-    {{ -0.5f, -0.5f, 0.0f }, { 1.0f, 0.0f, 0.0f }, { 0.0f, 0.0f }},
-    {{  0.5f, -0.5f, 0.0f }, { 0.0f, 1.0f, 0.0f }, { 1.0f, 0.0f }},
-    {{  0.5f,  0.5f, 0.0f }, { 0.0f, 0.0f, 1.0f }, { 1.0f, 1.0f }},
-    {{ -0.5f,  0.5f, 0.0f }, { 1.0f, 1.0f, 1.0f }, { 0.0f, 1.0f }},
+    // Bottom-left
+    {{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
+    // Bottom-right  
+    {{ 0.5f, -0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},
+    // Top-right
+    {{ 0.5f,  0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},
+    // Top-left
+    {{-0.5f,  0.5f, 0.0f}, {1.0f, 1.0f, 1.0f}, {0.0f, 1.0f}},
 };
- 
-const std::vector<u32> indices = { 0, 1, 2, 2, 3, 0 };
+
+// Indices for two triangles forming the square
+const std::vector<uint32_t> indices = {
+    0, 1, 2,  // First triangle (bottom-left, bottom-right, top-right)
+    0, 2, 3   // Second triangle (bottom-left, top-right, top-left)
+};
  
 Render::Render(Vulkan_Context& ctx, Platform& platform)
 {
